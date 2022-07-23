d87d9079b1f2d35a18467f41df9ab58198aeac4a
verevar <101432099+verevar@users.noreply.github.com>
2022-07-23 10:25:36 -0500

    new json

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,38 @@
-var years = ['2014', '2015', '2016']
-
-Plotly.d3.csv('focused_df.csv', (err, rows) => {
-  var data = years.map(y => {
-    var d = rows.filter(r => r.Year === y)
-    
-    return {
-      type: 'bar',
-      name: y,
-      x: d.map(r => r.Injury),
-      y: d.map(r => r.Country)
-    }
-  })
-  
-  Plotly.newPlot('graph', data)
-})
-Run Pen
-
-
-Resources
+import csv
+import json
+ 
+ 
+# Function to convert a CSV to JSON
+# Takes the file paths as arguments
+def make_json(csvFilePath, jsonFilePath):
+     
+    # create a dictionary
+    data = {}
+     
+    # Open a csv reader called DictReader
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+         
+        # Convert each row into a dictionary
+        # and add it to data
+        for rows in csvReader:
+             
+            # Assuming a column named 'No' to
+            # be the primary key
+            key = rows['No']
+            data[key] = rows
+ 
+    # Open a json writer, and use the json.dumps()
+    # function to dump data
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+         
+# Driver Code
+ 
+# Decide the two file paths according to your
+# computer system
+csvFilePath = r'focused_df.csv'
+jsonFilePath = r'sample.json'
+ 
+# Call the make_json function
+make_json(csvFilePath, jsonFilePath)
